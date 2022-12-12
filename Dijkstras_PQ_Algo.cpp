2cfa5b85cc1e6f47b717aa1611100ac9bb7cd43b
Balakrishnan000 <rbk.raj.000@gmail.com>
2022-12-12 22:57:10 +0530

    Commit-2

--- Dijkstras_PQ_Algo.cpp
+++ Dijkstras_PQ_Algo.cpp
@@ -1,3 +1,9 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
 #define PII pair<int,int>
 class Solution
 {
@@ -40,3 +46,44 @@ class Solution
         // we simply 6 + 2 = 8 < 4 BIG NO Always as the 8 came from 4 + 2 + 2
     }
 };
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
